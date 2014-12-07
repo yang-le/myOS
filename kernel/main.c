c64e2ab78a3686408ab54db77b8ca46a87550b03
yangle <qtxzh0125@gmail.com>
2014-12-07 21:21:37 +0800

    a new demo

--- kernel/main.c
+++ kernel/main.c
@@ -1,14 +1,38 @@
+#include <bios.h>
 #include <char.h>
 
 void my_os(void)
 {
-//	char_init();
-	char str[] = "hello world!";
+	char str[] = "hello world!\r\n\r\n";
 	puts(str);
+	
+	char color[][16] = {
+		"black\r\n",
+		"blue\r\n",
+		"green\r\n",
+		"cyan\r\n",
+		"red\r\n",
+		"magenta\r\n",
+		"yellow\r\n",		
+		"white\r\n"	
+	};
+	
+	union disp_attrib attrib = {0};
+	struct cursor_info info = {0};
+	int i = 0;
+	
+	// give you some color to see see :-)
+	for (; i <= COLOR_WHITE; ++i) {
+		get_cursor_info(0, &info);
+		attrib.fcolor = i;
+		attrib.bcolor = COLOR_WHITE - i;
+		tele_string(color[i], strlen(color[i]), info.row, info.col, 0, MODE_UPDATE_CURSOR, attrib);
+	}
+
 	for(;;){
 		delay(500);
-		set_cursor_shape(0, 15, 0);
+		set_cursor_shape(info.start, info.end, 0);
 		delay(500);
-		set_cursor_shape(0, 15, 1);
+		set_cursor_shape(info.start, info.end, 1);
 	}
 }
