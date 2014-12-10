24d50129ab4d30f42a09bfba59900a98876a5e88
yang-le <qtxzh0125@gmail.com>
2014-12-10 23:37:23 +0800

    fix script error

--- boot/fixsize.sh
+++ boot/fixsize.sh
@@ -5,7 +5,7 @@ ELF=`file foo.out | grep -i elf`
 rm -f foo.out
 
 # elf don't like "_"
-if [ $ELF ]; then
+if [ "$ELF" ]; then
 SCMD="-e s/_my_os/my_os/"
 fi
 
@@ -13,7 +13,7 @@ if [ -s "../bin.size" ]; then
 	SIZE=`cat ../bin.size`
 	NUM=`expr $SIZE / 512 + 1`
 	sed -e "/SED FIXME START/,/SED FIXME END/c\movb \$$NUM, %al" $SCMD $1
-elif [ $ELF ]; then
+elif [ "$ELF" ]; then
 	sed $SCMD $1
 else
 	cat $1
